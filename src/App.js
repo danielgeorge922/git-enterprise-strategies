233e52e23b533a2dbe80aa3a75a14f6ceab70c04
samilieberman <samilieberman1@gmail.com>
2022-11-06 19:28:41 -0500

    add hello world message

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Daniel and this is my project </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
       </header>
     </div>
   );
