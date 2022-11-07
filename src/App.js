dbd8f1393115cc35603e2defc3858868338de8a7
samilieberman <samilieberman1@gmail.com>
2022-11-06 19:46:16 -0500

    Add new text and gator image

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Sami and this is my project </p>
       </header>
     </div>
   );
